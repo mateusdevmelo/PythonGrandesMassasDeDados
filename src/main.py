06a9beaa1969e425f4c00ca8c75577923a641571
Mateus Melo <mateusdevmelo@gmail.com>
2024-04-09 19:44:28 -0300

    second commit

--- src/main.py
+++ src/main.py
@@ -4,3 +4,21 @@ import glob
 
 # caminho para ler os arquivos
 folder_path = 'src\\data\\raw'
+
+# listar todos os arquivos do excel
+excel_files = glob.glob(os.path.join(folder_path,'*.xlsx'))
+
+if not excel_files:
+    print('Nenhum arquivo compátivel encontrado')
+else:
+    
+    dfs = []
+    
+    for excel_file in excel_files:
+        
+        try:
+            df_temp = pd.read_excel(excel_file) 
+            print(df_temp)
+               
+        except Exception as e:
+            print(f'Erro ao ler o arquivo`{excel_file} : {e}')
