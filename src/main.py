ec1f8faadc340d69e55e9f010eb5b4741007a1d3
Mateus Melo <mateusdevmelo@gmail.com>
2024-04-11 21:09:50 -0300

    third commit

--- src/main.py
+++ src/main.py
@@ -17,8 +17,23 @@ else:
     for excel_file in excel_files:
         
         try:
+            # ler o arquivo do excel
             df_temp = pd.read_excel(excel_file) 
-            print(df_temp)
+            
+            # pegar o nome do arquivo
+            file_name = os.path.basename(excel_file)
+            
+            if 'brasil' in file_name.lower():
+                df_temp['location'] = 'br'
+            elif 'france' in file_name.lower():
+                df_temp['location'] = 'fr'
+            elif 'italian' in file_name.lower():
+                df_temp['location'] = 'it'
                
+            # criar uma nova coluna chamada campaign
+            df_temp['campaign'] = df_temp['utm_link'].str.extract(r'utm_campaign=(.*)')   
+            
+            print(df_temp)
+            
         except Exception as e:
             print(f'Erro ao ler o arquivo`{excel_file} : {e}')
